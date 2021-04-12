82e40e9fc0cc4811834083b0f03f31d58338929c
cjhjhj <cjhjhj@gmail.com>
2021-04-12 15:41:36 -0500

    Top-down RT assignment

--- topDownFeatureAssignment.py
+++ topDownFeatureAssignment.py
@@ -2,15 +2,51 @@ import sys, os, re, utils, numpy as np, pandas as pd
 from pyteomics import mzxml
 from featureDetection import detectFeatures
 
+
+def getMs2ToSurvey(reader):
+    # Read mzXML file and make a relationship between every MS2 and its survey scan
+    print("  Read a mzXML file: to find survey scans of MS2 scans")
+    res = {}
+    for spec in reader:
+        if spec["msLevel"] == 1:
+            survey = int(spec["num"])
+        elif spec["msLevel"] == 2:
+            res[int(spec["num"])] = survey
+    print("  Done ...\n")
+    return res
+
+
+def getPrecursorPeak(reader, psmScanNumber, surveyScanNumber, params):
+    isoWindow = float(params["isolation_window"])
+
+    # Find the precursor peak of the PSM (the strongest peak within the isolation window)
+    nominalPrecMz = float(re.search("([0-9.]+)\\@", reader[str(psmScanNumber)]["filterLine"]).group(1))
+    mzArray = reader[str(surveyScanNum)]["m/z array"]
+    intArray = reader[str(surveyScanNum)]["intensity array"]
+    ind = (mzArray >= nominalPrecMz - 0.5 * isoWindow) & (mzArray <= nominalPrecMz + 0.5 * isoWindow)
+    if sum(ind > 0):
+        subMzArray = mzArray[ind]
+        subIntArray = intArray[ind]
+        ind2 = np.argmax(subIntArray)
+        precMz = subMzArray[ind2]
+        precIntensity = subIntArray[ind2]
+    else:
+        precMz = -1
+        precIntensity = -1
+
+    precRt = reader[str(surveyScanNum)]["retentionTime"] * 60   # Unit of second
+    return precMz, precIntensity, precRt
+
+
 # Input
 # 1. mzXML file
 # 2. ID.txt file containing all identified PSMs
-# args = sys.argv
-# del args[0]
-# featureFile = args[0]
-# idTxt = args[1:]
-mzXML = "../Data/FTLD_Batch2_F50.mzXML"
-idTxt = "../Data/ID.txt"
+args = sys.argv
+del args[0]
+mzXML = args[0]
+idTxt = args[1:]
+# mzXML = "../Data/FTLD_Batch2_F50.mzXML"
+# idTxt = "../Data/ID.txt"
 
 # Feature detection
 params = {}
@@ -24,21 +60,13 @@ params["max_percentage_RT_range"] = "100"    # threshold maximum percentage of t
 params["min_peak_intensity"] = "10000"       # threshold of a peak intensity
 params["skipping_scans"] = "3"               # number of skipping scans during 3D formation
 params["mass_tolerance_peak_matching"] = "3" # mass tolerance for peak matching during 3D formation
-# features, ms1ToFeatures = detectFeatures(mzXML, params)
-features = pd.read_pickle("FTLD_Batch2_F50_Features_SN0_Gap3_3ppm.pickle")
-ms1ToFeatures = pd.read_pickle("FTLD_Batch2_F50_MS1_to_Features.pickle")
-print()
+features, ms1ToFeatures = detectFeatures(mzXML, params)
+# features = pd.read_pickle("FTLD_Batch2_F50_Features_SN0_Gap3_3ppm.pickle")
+# ms1ToFeatures = pd.read_pickle("FTLD_Batch2_F50_MS1_to_Features.pickle")
 
 # Read mzXML file
-print("  Read a mzXML file: to find survey scans of MS2 scans")
-ms2ToSurvey = {}
 reader = mzxml.MzXML(mzXML)
-for spec in reader:
-    if spec["msLevel"] == 1:
-        survey = int(spec["num"])
-    elif spec["msLevel"] == 2:
-        ms2ToSurvey[int(spec["num"])] = survey
-print("  Done ...\n")
+ms2ToSurvey = getMs2ToSurvey(reader)
 
 # Read ID.txt files to extract PSM information
 print("  Read ID.txt file and feature file")
@@ -53,104 +81,68 @@ psms = psms.drop_duplicates()
 print("  PSM information has been parsed\n")
 
 # Find the match between features and PSMs
+n1, n2 = 0, 0   # n1 = number of PSMs mapped to feature(s), n2 = number of PSMs not mapped to any feature
+isoWindow = 1   # Isolation window size for a precursor peak
 proton = 1.007276466812
-featureName = "FTLD_Batch2_F50"
-# featureName = os.path.basename(featureFile).split(".")[0]
-n1, n2, n3 = 0, 0, 0
+featureName = os.path.basename(mzXML).split(".")[0]
 progress = utils.progressBar(psms.shape[0])
 for idx, psm in psms.iterrows():
     progress.increment()
     [psmRunName, psmScanNum, _, psmZ, _] = os.path.basename(psm["Outfile"]).split(".")
     psms.loc[idx, "charge"] = psmZ
     if psmRunName == featureName:
-        # Find the precursor peak of the PSM
-        precMz = float(re.search("([0-9.]+)\\@", reader[psmScanNum]["filterLine"]).group(1))
-        surveyScanNum = ms2ToSurvey[int(psmScanNum)]
-        mzArray = reader[str(surveyScanNum)]["m/z array"]
-        intArray = reader[str(surveyScanNum)]["intensity array"]
-        ii = np.where((mzArray >= precMz - 0.5) & (mzArray <= precMz + 0.5))
-        if len(ii[0] > 0):
-            subMzArray = mzArray[ii]
-            subIntArray = intArray[ii]
-            jj = np.argmax(subIntArray)
-            precMz = subMzArray[jj]
+        # Extract the precursor m/z
+        psmScanNum = int(psmScanNum)
+        surveyScanNum = ms2ToSurvey[psmScanNum]
+        precMz, _, _ = getPrecursorPeak(reader, psmScanNum, surveyScanNum, params)
+
+        # Assign the feature corresponding to the PSM, based on the precursor m/z and feature's m/z
+        if precMz != -1:
             psms.loc[idx, "precMz"] = precMz
             fInd = ms1ToFeatures[((ms1ToFeatures["ms1ScanNumber"] == str(surveyScanNum)) & (ms1ToFeatures["peakMz"] == precMz))]["featureIndex"].values
         else:
             fInd = []
-            print(psm)
-
-        if len(fInd) == 1:
-            fInd = fInd[0]
+        if len(fInd) > 0:   # Using
             psms.loc[idx, "featureIndex"] = fInd
             n1 += 1
-        elif len(fInd) > 1:
-            n2 += 1
         else:
-            n3 += 1
+            n2 += 1
     else:
         continue
+# print("There are {} unique PSMs".format(len(psms)))
+# print("{} PSMs match to unique features".format(n1))
+# print("{} PSMs do not match to any feature".format(n2))
 
-print("There are {} unique PSMs".format(len(psms)))
-print("{} PSMs match to unique features".format(n1))
-print("{} PSMs match to multiple features".format(n2))
-print("{} PSMs do not match to any feature".format(n3))
+# psms = pd.read_csv("psms_test.txt", sep="\t")
+# features = pd.read_pickle("FTLD_Batch2_F50_Features_SN0_Gap3_3ppm.pickle")
+# ms1ToFeatures = pd.read_pickle("FTLD_Batch2_F50_MS1_to_Features.pickle")
 
-
-# For the peptides mapped to features, their mapping pattern/category is determined
-subDf = psms[psms["featureIndex"] >=0][["Peptide", "charge", "featureIndex"]] # Sub-DataFrame for the peptides mapped to features
-subDf = subDf.drop_duplicates()
-pepDict = {}
-for i, row in subDf.iterrows():
-    pep = row["Peptide"] + "_" + row["charge"]
-    fInd = row["featureIndex"]
-    if pep in pepDict:
-        pepDict[pep].append(fInd)
+# Assign RT values to PSMs
+res = []
+keys = psms["Peptide"] + "_" + psms["charge"].astype(str)
+keys = list(set(keys))  # Keep unique "key"s
+for key in keys:
+    pep, z = key.split("_")
+    fInd = psms[(psms["Peptide"] == pep) & (psms["charge"] == int(z))]["featureIndex"].drop_duplicates().dropna()
+    if len(fInd) > 0:   # There is/are feature(s) corresponding to the key (i.e., peptide-charge pair)
+        # Top-down RT (RT derived from features)
+        rtArray = features.loc[fInd]["RT"].values
+        intArray = features.loc[fInd]["intensity"].values
     else:
-        pepDict[pep] = [fInd]
+        # Bottom-up RT (RT derived from PSM(s) of the key)
+        df = psms[(psms["Peptide"] == pep) & (psms["charge"] == int(z))]
+        rtArray = np.array([])
+        intArray = np.array([])
+        for ind, psm in df.iterrows():
+            [_, psmScanNum, _, _, _] = os.path.basename(psm["Outfile"]).split(".")
+            psmScanNum = int(psmScanNum)
+            surveyScanNum = ms2ToSurvey[psmScanNum]
+            _, precIntensity, precRt = getPrecursorPeak(reader, int(psmScanNum), surveyScanNum, params)
+            rtArray = np.append(rtArray, precRt)
+            intArray = np.append(intArray, precIntensity)
+    rt = sum(rtArray * intArray) / sum(intArray) / 60
+    rtStd = np.std(rtArray / 60)
+    res.append([key, rt, rtStd])
 
-n1, n2, n3 = 0, 0, 0
-p1, p2, p3 = [], [], []
-f1, f2, f3 = [], [], []
-for k, v in pepDict.items():
-    if len(v) == 1:
-        v = v[0]
-        ll = np.where(subDf["featureIndex"] == v)[0]
-        if len(ll) == 1:    # One-to-one (peptide-to-feature)
-            n1 += 1
-            p1.append(k)
-            f1.append(v)
-        else:   # Many-to-one
-            n2 += 1
-            p2.append(k)
-            f2.append(v)
-    else:   # One-to-many
-        n3 += 1
-        p3.append(k)
-        f3.extend(v)
-
-print("One-to-one: peptide-to-feature: {}".format(n1))
-print("Many-to-one: peptide-to-feature: {}".format(n2))
-print("One-to-many: peptide-to-feature: {}".format(n3))
-f1 = list(set(f1))
-f2 = list(set(f2))
-f3 = list(set(f3))
-p1 = list(set(p1))
-p2 = list(set(p2))
-p3 = list(set(p3))
-
-for k in p1:
-    [pep, z] = k.split("_")
-    psms.loc[(psms["Peptide"] == pep) & (psms["charge"] == z) & (psms["featureIndex"] >= 0), "category"] = "one-to-one"
-for k in p2:
-    [pep, z] = k.split("_")
-    psms.loc[(psms["Peptide"] == pep) & (psms["charge"] == z) & (psms["featureIndex"] >= 0), "category"] = "many-to-one"
-for k in p3:
-    [pep, z] = k.split("_")
-    psms.loc[(psms["Peptide"] == pep) & (psms["charge"] == z) & (psms["featureIndex"] >= 0), "category"] = "one-to-many"
-
-# psms.loc[(psms["Peptide"].isin(p1)) & (psms["featureIndex"] >= 0), "category"] = "one-to-one"
-# psms.loc[(psms["Peptide"].isin(p2)) & (psms["featureIndex"] >= 0), "category"] = "many-to-one"
-# psms.loc[(psms["Peptide"].isin(p3)) & (psms["featureIndex"] >= 0), "category"] = "one-to-many"
-
-psms.to_csv("psms.txt", sep="\t", index=False)
+res = pd.DataFrame(res, columns=["peptide_charge", "RT", "stdRT"])
+res.to_csv("FTLD_Batch2_F50_RT_assignment.txt", sep="\t", index=False)
